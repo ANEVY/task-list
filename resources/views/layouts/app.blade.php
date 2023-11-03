ff94c900c27ae1042d6585d8d88edc5a0b0039e6
Akombo Neville Akwo <akomboneville55@gmail.com>
2023-11-03 04:34:05 +0100

    added Alpine.js

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,11 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Laravel task list app</title>
     <script src="https://cdn.tailwindcss.com"></script>
+    <script src="//unpkg.com/alpinejs" defer></script>
+    {{--blade-formatter-disable--}}
+    <style></style>
+    {{--blade-formatter-enable}}
+
 
 </head>
 <body class="container mx-auto mt-10">
